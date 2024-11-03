ba9641642c2a2547744bd07898d4edec6ff1886a
Harsimran Singh Barki <30506261+HarsimranBarki@users.noreply.github.com>
2024-11-03 18:46:12 +0530

    Fixed hydration error and swiper buttons (#34)

--- src/slices/Stats/index.tsx
+++ src/slices/Stats/index.tsx
@@ -1,24 +1,21 @@
-import Stats from "@/components/Stats";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import Stats from '@/components/Stats'
+import { Content } from '@prismicio/client'
+import { SliceComponentProps } from '@prismicio/react'
 
 /**
  * Props for `Slices`.
  */
-export type SlicesProps = SliceComponentProps<Content.SlicesSlice>;
+export type SlicesProps = SliceComponentProps<Content.SlicesSlice>
 
 /**
  * Component for "Slices" Slices.
  */
 const Slices = ({ slice }: SlicesProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <Stats statsData={slice.primary.statscollection} />
-    </section>
-  );
-};
+    return (
+        <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+            <Stats statsData={slice.primary.statscollection} />
+        </section>
+    )
+}
 
-export default Slices;
+export default Slices
