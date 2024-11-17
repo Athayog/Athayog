a276f473bc90bdbd262168320cc9a2d9d17dc147
Athayog <77162718+Athayog@users.noreply.github.com>
2024-11-17 23:58:08 +0530

    Update ResponsiveImage

--- src/components/_shared/ResponsiveImage.tsx
+++ src/components/_shared/ResponsiveImage.tsx
@@ -16,7 +16,7 @@ const ResponsiveImage: React.FC<ResponsiveImageProps> = ({
             src={src}
             alt={alt}
             fill
-            style={{ objectFit: 'cover' }}
+            style={{ objectFit: 'contain' }}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             {...props}
         />
