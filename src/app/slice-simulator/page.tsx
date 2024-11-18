4d81350e1a06c1de984016ca6b02633d3b278137
Athayog <77162718+Athayog@users.noreply.github.com>
2024-11-18 18:24:59 +0530

    BugFix | Bullet List (#56)
    Co-authored-by: HarsimranBarki <harsimransinghbarki@gmail.com>

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,24 +1,16 @@
-import {
-    SliceSimulator,
-    SliceSimulatorParams,
-    getSlices,
-} from '@slicemachine/adapter-next/simulator'
+import { SliceSimulator, SliceSimulatorParams, getSlices } from '@slicemachine/adapter-next/simulator'
 import { SliceZone } from '@prismicio/react'
 import { redirect } from 'next/navigation'
 
 import { components } from '@/slices'
 
-export default function SliceSimulatorPage({
-    searchParams,
-}: SliceSimulatorParams & { searchParams: { secret?: string } }) {
-    if (
-        process.env.SLICE_SIMULATOR_SECRET &&
-        searchParams.secret !== process.env.SLICE_SIMULATOR_SECRET
-    ) {
+export default async function SliceSimulatorPage({ searchParams }: SliceSimulatorParams & { searchParams: { secret?: string } }) {
+    const params = await searchParams
+    if (process.env.SLICE_SIMULATOR_SECRET && searchParams.secret !== process.env.SLICE_SIMULATOR_SECRET) {
         redirect('/')
     }
 
-    const slices = getSlices(searchParams.state)
+    const slices = getSlices(params.state)
 
     return (
         <SliceSimulator>
