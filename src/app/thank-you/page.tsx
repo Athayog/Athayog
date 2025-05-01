f1d5453d94b900b6b1c111d2e6179228a35022d3
Harsimran <Harsimran>
2025-05-01 17:08:18 +0530

    updated form

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import Thanks from '@/app/thank-you/thanks'
 
-
 export const metadata: Metadata = {
     title: 'Thank You | Athayog',
     description: 'Thank you for your submission',
