0c2069b87ed5ad15c7f8e7c46c917c0ee46012cf
Harsimran Singh Barki <30506261+HarsimranBarki@users.noreply.github.com>
2025-06-02 22:18:26 +0530

    Changes | Disable Navigation Yoga Arambha  (#87)
    Co-authored-by: Harsimran <Harsimran>

--- src/components/_header/index.tsx
+++ src/components/_header/index.tsx
@@ -23,6 +23,7 @@ const Navbar: React.FC = () => {
     const { user } = useAuthStore()
     const [gradient, setGradient] = useState('linear-gradient(to bottom, rgba(0, 0, 0, 0.95) 0%, rgba(0, 0, 0, 0) 100%)')
 
+
     const handleClick = (event: React.MouseEvent<HTMLElement>, index: number) => {
         // Update the anchor element for the clicked menu
         setAnchorEls((prev) => ({
@@ -51,6 +52,7 @@ const Navbar: React.FC = () => {
         return unsubscribe
     }, [initializeAuth])
 
+
     return (
         <AppBar
             position="fixed"
@@ -59,6 +61,10 @@ const Navbar: React.FC = () => {
                 boxShadow: 'none',
                 padding: isScrolled ? '10px 20px' : '25px 20px',
                 transition: 'padding 0.5s ease-in-out, background 0.5s ease-in-out',
+                ...(pathname === '/yoga-arambha-25' && {
+                    pointerEvents: 'none',
+
+                }),
             }}
         >
             <ScrollListener />
@@ -74,66 +80,72 @@ const Navbar: React.FC = () => {
                     </Link>
                 </Box>
 
-                <NavContainer>
-                    {navItems.map(({ label, path, type, children }, index) => {
-                        if (type === 'nav') {
-                            return (
-                                <Link href={path || '/'} passHref={true} key={index}>
-                                    <NavLinkButton variant="text" pathname={pathname} path={path || '/'} navigationVariant={navigationVariant}>
-                                        {label}
-                                    </NavLinkButton>
-                                </Link>
-                            )
-                        } else if (type === 'menu' && children) {
-                            return (
-                                <React.Fragment key={index}>
-                                    <MenuButton variant="text" aria-controls={anchorEls[index] ? `submenu-${index}` : undefined} aria-haspopup="true" onClick={(event) => handleClick(event, index)}>
-                                        {label}
-                                    </MenuButton>
-                                    <Menu
-                                        id={`submenu-${index}`}
-                                        anchorEl={anchorEls[index]}
-                                        open={Boolean(anchorEls[index])}
-                                        onClose={() => handleClose(index)}
-                                        disableScrollLock={true}
-                                        MenuListProps={{
-                                            'aria-labelledby': `submenu-button-${index}`,
+                {pathname !== '/yoga-arambha-25' &&
+                    <NavContainer>
+                        {navItems.map(({ label, path, type, children }, index) => {
+                            if (type === 'nav') {
+                                return (
+                                    <Link href={path || '/'} passHref={true} key={index}>
+                                        <NavLinkButton variant="text" pathname={pathname} path={path || '/'} navigationVariant={navigationVariant}>
+                                            {label}
+                                        </NavLinkButton>
+                                    </Link>
+                                )
+                            } else if (type === 'menu' && children) {
+                                return (
+                                    <React.Fragment key={index}>
+                                        <MenuButton variant="text" aria-controls={anchorEls[index] ? `submenu-${index}` : undefined} aria-haspopup="true" onClick={(event) => handleClick(event, index)}>
+                                            {label}
+                                        </MenuButton>
+                                        <Menu
+                                            id={`submenu-${index}`}
+                                            anchorEl={anchorEls[index]}
+                                            open={Boolean(anchorEls[index])}
+                                            onClose={() => handleClose(index)}
+                                            disableScrollLock={true}
+                                            MenuListProps={{
+                                                'aria-labelledby': `submenu-button-${index}`,
+                                            }}
+                                        >
+                                            {RenderMenuItems(children, () => handleClose(index), handleSubMenuClick, subMenuAnchorEl)}
+                                        </Menu>
+                                    </React.Fragment>
+                                )
+                            }
+                        })}
+                    </NavContainer>
+                }
+
+                {pathname !== '/yoga-arambha-25' &&
+                    <>
+                        <TrialAndAuth>
+                            {!user && (
+                                <Link href="/login">
+                                    <Button
+                                        sx={{
+                                            backgroundColor: '#47820D',
+                                            color: '#FFFFFF',
+                                            padding: '15px, 25px',
+                                            fontSize: '18px',
+                                            fontWeight: '700',
+                                            width: '115px',
                                         }}
                                     >
-                                        {RenderMenuItems(children, () => handleClose(index), handleSubMenuClick, subMenuAnchorEl)}
-                                    </Menu>
-                                </React.Fragment>
-                            )
-                        }
-                    })}
-                </NavContainer>
+                                        Login
+                                    </Button>
+                                </Link>
+                            )}
+                            <Link href={'/trial-classes'} passHref={true}>
+                                <TrialButton variant="text">
+                                    Get a<span>Free Trial</span>
+                                </TrialButton>
+                            </Link>
+                            <AccountMenu />
+                        </TrialAndAuth>
 
-                <TrialAndAuth>
-                    {!user && (
-                        <Link href="/login">
-                            <Button
-                                sx={{
-                                    backgroundColor: '#47820D',
-                                    color: '#FFFFFF',
-                                    padding: '15px, 25px',
-                                    fontSize: '18px',
-                                    fontWeight: '700',
-                                    width: '115px',
-                                }}
-                            >
-                                Login
-                            </Button>
-                        </Link>
-                    )}
-                    <Link href={'/trial-classes'} passHref={true}>
-                        <TrialButton variant="text">
-                            Get a<span>Free Trial</span>
-                        </TrialButton>
-                    </Link>
-                    <AccountMenu />
-                </TrialAndAuth>
-
-                <MobileDrawer />
+                        <MobileDrawer />
+                    </>
+                }
             </Toolbar>
         </AppBar>
     )
