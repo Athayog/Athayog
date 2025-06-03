34ce1bbdad26991102c8882ecf6f431538247445
Harsimran Singh Barki <30506261+HarsimranBarki@users.noreply.github.com>
2025-06-03 19:01:37 +0530

    Feature | Facebook Pixel (#88)
    * added pixel
    
    * added keys
    
    * removed id
    
    ---------
    
    Co-authored-by: Harsimran <Harsimran>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,31 @@ export default function RootLayout({
             <head>
                 <link rel="icon" href="/favicon.ico" sizes="any" />
                 <meta name="google-site-verification" content="MFdD5TUc66yWX-w0hwFHmVkJWyt8BAkzk-g3jR4KLlo" />
-                <GoogleTagManager gtmId="GTM-N4LH3M3" />
+                <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID || ''} />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL});
+                fbq('track', 'PageView');
+            `,
+                    }}
+                />
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL}&ev=PageView&noscript=1`}
+                    />
+                </noscript>
             </head>
             <body >
                 <AppRouterCacheProvider>
